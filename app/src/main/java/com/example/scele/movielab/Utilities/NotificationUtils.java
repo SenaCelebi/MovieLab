89f95aa7891d77794b7bbca145d20f21a5ad3595
Melike Doğru <dogrumelike98@gmail.com>
2020-01-05 15:22:53 +0300

    Actions added

--- app/src/main/java/com/example/scele/movielab/Utilities/NotificationUtils.java
+++ app/src/main/java/com/example/scele/movielab/Utilities/NotificationUtils.java
@@ -15,12 +15,16 @@ import android.support.v4.content.ContextCompat;
 
 import com.example.scele.movielab.HomeActivity;
 import com.example.scele.movielab.R;
+import com.example.scele.movielab.SearchActivity;
 
 public class NotificationUtils {
 
     private static final int MOVIE_REMINDER_NOTIFICATION_ID  = 1138;
     private static final int MOVIE_REMINDER_PENDING_INTENT_ID       = 3417;
     private static final String MOVIE_REMINDER_NOTIFICATION_CHANNEL_ID = "reminder_notification_channel";
+
+    private static final int    ACTION_IGNORE_PENDING_INTENT_ID        = 14;
+    private static final int ACTION_DRINK_PENDING_INTENT_ID = 1;
 
     public static void clearAllNotifications(Context context) {
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
@@ -40,6 +44,14 @@ public class NotificationUtils {
             notificationManager.createNotificationChannel(mChannel);
         }
 
+        Intent yesIntent = new Intent(context, SearchActivity.class);
+        yesIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent yesPendingIntent = PendingIntent.getActivity(context,ACTION_DRINK_PENDING_INTENT_ID, yesIntent, PendingIntent.FLAG_ONE_SHOT);
+
+        Intent noIntent = new Intent(context, HomeActivity.class);
+        noIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent noPendingIntent = PendingIntent.getActivity(context,ACTION_IGNORE_PENDING_INTENT_ID, noIntent, PendingIntent.FLAG_ONE_SHOT);
+
         NotificationCompat.Builder notificationBuilder = new NotificationCompat
                 .Builder(context, MOVIE_REMINDER_NOTIFICATION_CHANNEL_ID);
         notificationBuilder
@@ -51,10 +63,11 @@ public class NotificationUtils {
                 .setStyle(new NotificationCompat.BigTextStyle().bigText("Do you want to check movies"))
                 .setDefaults(Notification.DEFAULT_VIBRATE)
                 .setContentIntent(contentIntent(context))
-                .setAutoCancel(true);
-        /*notificationBuilder
-                .addAction(yesCheck(context))
-                .addAction(noCheck());*/
+                .setAutoCancel(true)
+        .addAction(R.drawable.ic_play_arrow_black_24dp,"YES", yesPendingIntent )
+        .addAction(R.drawable.ic_play_arrow_black_24dp, "NO", noPendingIntent);
+        
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN && Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
             notificationBuilder.setPriority(NotificationCompat.PRIORITY_HIGH);
         }
@@ -68,18 +81,6 @@ public class NotificationUtils {
         return largeIcon;
     }
 
-
-    private static final int    ACTION_IGNORE_PENDING_INTENT_ID        = 14;
-
-   /* private static Notification.Action yesCheck(Context context){
-
-    }
-
-    private static final int ACTION_DRINK_PENDING_INTENT_ID = 1;
-
-    private static Notification.Action noCheck(Context context){
-
-    }*/
 
     private static PendingIntent contentIntent(Context context) {
         Intent startActivityIntent = new Intent(context, HomeActivity.class);
